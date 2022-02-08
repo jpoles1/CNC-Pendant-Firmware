9eafcbaa32e5774fe6296a1fcafbc3587660711f
Jordan Poles <jpdev.noreply@gmail.com>
2022-02-08 00:14:33 -0500

    The encoder approach

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <JCNC.h>
 #include <BluetoothSerial.h>
+#include <ESP32Encoder.h>
 
-int rot_pin_A = 26;
-int rot_pin_B = 25;
+int dial_pin_A = 26;
+int dial_pin_B = 25;
 int switch_pin_A = 32;
 int switch_pin_B = 33;
 int top_left_key_pin = 15;
@@ -10,11 +11,13 @@ int bot_left_key_pin = 22;
 int top_right_key_pin = 17;
 int bot_right_key_pin = 21;
 int estop_pin = 13;
+int batt_adc_en_pin = 14; //ADC_EN is the ADC detection enable port
+int batt_adc_pin = 34; 
 
 int steps = 0;
 
 unsigned long last_update = 0; // time of last update in ms
-static int update_timeout = 100; // time to wait from last update before sending move command to host/cnc
+static int update_timeout = 250; // time to wait from last update before sending move command to host/cnc
 
 unsigned long top_left_key_last = 0;
 unsigned long bot_left_key_last = 0;
@@ -26,8 +29,11 @@ static int btn_debounce = 250;
 
 JCNC cnc; // Init JCNC
 BluetoothSerial SerialBT;
+ESP32Encoder dial;
 
-const char *pin = "4424";
+uint8_t bt_address[6]  = {0x00, 0x18, 0xE4, 0x40, 0x00, 0x06};
+const char *bt_pin = "4424";
+
 
 /*void handle_as_keyboard(char* gcode) {
 	if(!keyboard.isConnected()) {
@@ -38,24 +44,18 @@ const char *pin = "4424";
 	keyboard.println(gcode);
 }*/
 
+float read_batt_voltage() {
+	const int vref = 1100;
+	uint16_t v = analogRead(batt_adc_pin);
+	float battery_voltage = ((float)v / 4095.0) * 2.0 * 3.3 * (vref / 1000.0);
+}
+
 void handle_as_ble_grbl(char* gcode) {
 	/*if(!SerialBT.connected()) {
 		cnc.ui.logtft.add("BLE Discon");
 	}*/
 	SerialBT.println("G91");
 	SerialBT.println(gcode);
-
-}
-
-void rot_interrupt() {
-	//bool pin_A = digitalRead(rot_pin_A);
-	bool pin_B = digitalRead(rot_pin_B);
-	//Serial.print("Pin A: ");
-	//Serial.println(pin_A);
-	//Serial.print("Pin B: ");
-	//Serial.println(pin_B);
-	steps += pin_B ? 1 : -1;
-	last_update = millis();
 }
 
 void switch_interrupt() {
@@ -118,9 +118,39 @@ void btn_interrupt() {
 	}
 }
 
+void bt_watchdog(void * params) {
+	bool last_conn_state = 0;
+	for(;;) {
+		bool connected = SerialBT.connected(1000);
+		/*Serial.print("CONN STATUS: ");
+		Serial.println(connected);*/
+		if(!connected) {
+			if(last_conn_state){
+				cnc.ui.logtft.add("BT DISCONN");
+				last_conn_state = 0;
+			}
+			SerialBT.disconnect();
+			SerialBT.connect(bt_address);
+			bool connnect_attept = SerialBT.connected(10000);
+			if(connnect_attept) {
+				cnc.ui.logtft.add("BT CONN");
+				Serial.println("Connected Succesfully!");
+				last_conn_state = 1;
+			} else {
+				cnc.ui.logtft.add("BT CONN ERR");
+				Serial.println("Failed to connect!");
+			}
+		}
+		vTaskDelay(2.5 * 1000 / portTICK_PERIOD_MS);
+	}
+}
+
 void setup() {
-	pinMode(rot_pin_A, INPUT);
-	pinMode(rot_pin_B, INPUT);
+	Serial.begin(115200);
+	
+	ESP32Encoder::useInternalWeakPullResistors=DOWN;
+	dial.attachSingleEdge(dial_pin_A, dial_pin_B);
+
 	pinMode(switch_pin_A, INPUT_PULLUP);
 	pinMode(switch_pin_B, INPUT_PULLUP);
 
@@ -129,8 +159,13 @@ void setup() {
 	pinMode(top_right_key_pin, INPUT_PULLUP);
 	pinMode(bot_right_key_pin, INPUT_PULLUP);
 	pinMode(estop_pin, INPUT_PULLUP);
-	
-	Serial.begin(115200);
+	/*
+    ADC_EN is the ADC detection enable port
+    If the USB port is used for power supply, it is turned on by default.
+    If it is powered by battery, it needs to be set to high level
+    */
+    pinMode(batt_adc_en_pin, OUTPUT);
+    digitalWrite(batt_adc_en_pin, HIGH);
 
 	//Start TFT screen
 	cnc.ui.init();
@@ -141,29 +176,14 @@ void setup() {
 	//Start BLE Serial
 	if(!SerialBT.begin("JCNC Pendant", true)) {
 		Serial.println("An error occurred initializing Bluetooth");
-		cnc.ui.logtft.add("BLE INIT ERR");
+		cnc.ui.logtft.add("BT INIT ERR");
 	};
-	cnc.ui.logtft.add("BLE START");
-	//SerialBT.enableSSP();
-	SerialBT.setPin(pin);
-	//bool connected = SerialBT.connect("JCNC");
-	uint8_t bt_address[6]  = {0x00, 0x18, 0xE4, 0x40, 0x00, 0x06};
-	bool connected = SerialBT.connect(bt_address);
-
-	if(connected) {
-		cnc.ui.logtft.add("BLE CONNECT");
-		Serial.println("Connected Succesfully!");
-	} else {
-		while(!SerialBT.connected(15000)) {
-			cnc.ui.logtft.add("BLE CONN ERR");
-			Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app."); 
-		}
-	}
-
-	//Start rotary encoder
-	attachInterrupt(rot_pin_A, rot_interrupt, RISING);
-	//attachInterrupt(rot_pin_B, rot_interrupt, RISING);
-
+	cnc.ui.logtft.add("BT START");
+	
+	SerialBT.setPin(bt_pin);
+	
+	xTaskCreate(bt_watchdog, "bt_watchdog", 10000, NULL, 1, NULL);
+	
 	//Start rot switch
 	attachInterrupt(switch_pin_A, switch_interrupt, CHANGE);
 	attachInterrupt(switch_pin_B, switch_interrupt, CHANGE);
@@ -179,27 +199,28 @@ void setup() {
 	estop_interrupt();
 }
 
-void gen_gcode(char* gcode) {
+void gen_gcode(char* gcode, float steps) {
 	snprintf(gcode, 100, "G0 %c%.2f", cnc.current_axis, steps * cnc.move_mult());
 }
 
 void loop() {
 	if( cnc.estop ) {
 		cnc.draw();
-		steps = 0;
+		dial.clearCount();
 	} 
 	else if( (millis() - last_update) > update_timeout ) {
 		while (SerialBT.available()) {
 			Serial.write(SerialBT.read());
 		}
+		int steps = dial.getCount();
+		dial.clearCount();
 		if (steps != 0) {
 			//switch_interrupt();
 			char gcode[100];
-			gen_gcode(gcode);
+			gen_gcode(gcode, steps);
 			Serial.println(gcode);
 			cnc.ui.logtft.add(gcode);
 			handle_as_ble_grbl(gcode);
-			steps = 0;
 		}
 		last_update = millis(); // reset timer
 	}
